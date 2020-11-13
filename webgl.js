ef06687d615c49ca30630901367b0033eb311916
Mochamad Lucky Pradana <ampersanda.clj@icloud.com>
2020-11-13 20:15:31 +0700

    feat(video-3): passing variables to shader

--- webgl.js
+++ webgl.js
@@ -3,6 +3,7 @@ let gl,
 
 initGL();
 createShaders();
+createVertices();
 draw();
 
 function initGL() {
@@ -14,9 +15,12 @@ function initGL() {
 
 function createShaders() {
   let vertexShaderSource = "";
+  vertexShaderSource += "attribute vec4 coords;";
+  vertexShaderSource += "attribute float size;";
+  vertexShaderSource += '';
   vertexShaderSource += "void main(void) {";
   vertexShaderSource += "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);";
-  vertexShaderSource += "  gl_PointSize = 100.0;";
+  vertexShaderSource += "  gl_PointSize = size;";
   vertexShaderSource += "}";
 
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -24,8 +28,11 @@ function createShaders() {
   gl.compileShader(vertexShader);
 
   let fragmentShaderSource = "";
+  fragmentShaderSource += "precision mediump float;";
+  fragmentShaderSource += "uniform vec4 color;";
+  fragmentShaderSource += "";
   fragmentShaderSource += "void main(void) {";
-  fragmentShaderSource += "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);";
+  fragmentShaderSource += "  gl_FragColor = color;";
   fragmentShaderSource += "}";
 
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -37,6 +44,17 @@ function createShaders() {
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
+}
+
+function createVertices() {
+  const coords = gl.getAttribLocation(shaderProgram, 'coords');
+  gl.vertexAttrib3f(coords, 0.0, 0.0, 0.0);
+
+  const size = gl.getAttribLocation(shaderProgram, 'size');
+  gl.vertexAttrib1f(size, 100.0);
+
+  const color = gl.getUniformLocation(shaderProgram, 'color');
+  gl.uniform4f(color, 0, 1, 0, 1);
 }
 
 function draw() {
